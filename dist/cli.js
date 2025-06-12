6dbf5782ed24e89589a1f274157bbded45efcb59
zakhaya <zakaria.haya.1@ens.etsmtl.ca>
2025-06-11 18:18:11 -0700

    Correction: Suppression des fichiers générés et amélioration du .gitignore

--- dist/cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/cli.ts
-const fizzbuzz_1 = require("./fizzbuzz");
-for (let i = 1; i <= 100; i++) {
-    console.log(fizzbuzz_1.fizzBuzz(i));
-}
-//# sourceMappingURL=cli.js.map
